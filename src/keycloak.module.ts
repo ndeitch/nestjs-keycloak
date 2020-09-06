bd737f964664d423216fa91e306c579caacaa759
Natan Deitch <natan.luz.deitch@gmail.com>
2020-09-06 18:35:18 -0300

    refactor: module export

--- src/keycloak.module.ts
+++ src/keycloak.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { GraphQLModule } from '@nestjs/graphql'
-import { UserController } from './controller'
 import { KeycloakGuard } from './guard'
-import { UserResolver } from './resolver'
 import { KeycloakStrategy } from './strategy'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    GraphQLModule.forRoot({ autoSchemaFile: true, context: ({ req }) => req }),
-  ],
-  controllers: [UserController],
-  providers: [KeycloakGuard, KeycloakStrategy, UserResolver],
+  imports: [ConfigModule.forRoot()],
+  providers: [KeycloakGuard, KeycloakStrategy],
 })
 export class KeycloakModule {}
